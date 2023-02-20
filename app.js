485da89eda96d3591e35a17ac34327bfb9bd30f6
ChanduniNethmini <nchanduni21@gmail.com>
2023-02-20 11:16:25 +0530

    added db connection and  folder structure

---
+++ app.js
@@ -0,0 +1,38 @@
+//define dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+const PORT = 5000;
+const { MONGODBURI } = require("./keys");
+app.use(express.json());
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//   })
+// );
+
+ require("./models/user");
+// require("./models/post");
+
+ //register routes
+app.use(require("./routes/auth"));
+// app.use(require("./routes/post"));
+// app.use(require("./routes/user"));
+
+//establishing Database connection
+mongoose.connect(MONGODBURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("connection successful");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("connection failed", err);
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running on ${PORT}`);
+});
---
+++ models/user.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    pic:{
+        type:String,
+        default:"https://res.cloudinary.com/dt0isai38/image/upload/v1674850149/default_kmiptn.png"
+    }
+})
+
+mongoose.model('User', userSchema)
